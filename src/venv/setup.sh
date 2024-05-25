d58c617aebeb548eca22c762e69c277a26158907
Purinda Gunasekara <purinda@gmail.com>
2024-05-25 18:06:42 +1000

    Fixed ansible insallation issue where ansible-galaxy was erroring out when installing plugins

--- src/venv/setup.sh
+++ src/venv/setup.sh
@@ -29,10 +29,16 @@ function venv_setup() {
     is_ok
 
     # Then install with pip3 will be inside that virtual environment
-    out_n "Installing requirements.."
+    out_n "Installing base requirements.."
 
     # The ignore warnings is to squelch the InsecurePlatformWarnings that happen on some versions of Python
     PYTHONWARNINGS="ignore" \
-    pip3 install -qIr "${app}/var/pip-requirements.txt"
+    pip3 install --upgrade -qIr "${app}/var/pip-requirements.txt"
+    is_ok
+
+    out_n "Installing Ansible plugins.."
+    # Run ansible-galaxy collection install amazon.aws
+    ansible-galaxy collection install -i --upgrade amazon.aws > /dev/null 2>&1
+
     is_ok
 }
